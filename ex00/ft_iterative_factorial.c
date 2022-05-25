5f94a742e2e18ac18cb81b756560f1406db63465
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2022-05-25 10:23:56 +0200

    ex00 done

--- ex00/ft_iterative_factorial.c
+++ ex00/ft_iterative_factorial.c
@@ -6,21 +6,30 @@
 /*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 10:11:29 by yrabby            #+#    #+#             */
-/*   Updated: 2022/05/25 10:13:58 by yrabby           ###   ########.fr       */
+/*   Updated: 2022/05/25 10:23:45 by yrabby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_iterative_factorial(int nb)
 {
-	int	i;
-	int	ret;
+	long	i;
+	long	sign;
+	long	ret;
+	long	n;
 
+	n = (long)nb;
+	sign = 1;
+	if (n < 0)
+	{
+		sign = -1;
+		n *= -1;
+	}
 	i = 1;
 	ret = 1;
-	while (i < nb)
+	while (i <= n)
 	{
 		ret = i * ret;
 		++i;
 	}
-	return (ret);
+	return (ret * sign);
 }
