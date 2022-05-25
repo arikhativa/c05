f15c89b2f8227efd2c61d120e6b117fbeb329e0d
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2022-05-25 10:40:53 +0200

    ex01

--- ex01/ft_recursive_factorial.c
+++ ex01/ft_recursive_factorial.c
@@ -1,28 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/25 10:11:29 by yrabby            #+#    #+#             */
-/*   Updated: 2022/05/25 10:39:34 by yrabby           ###   ########.fr       */
+/*   Created: 2022/05/25 10:24:28 by yrabby            #+#    #+#             */
+/*   Updated: 2022/05/25 10:39:44 by yrabby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_iterative_factorial(int nb)
+int	rec(int nb)
 {
-	int	i;
-	int	ret;
+	if (!nb)
+		return (1);
+	return (rec(nb - 1) * nb);
+}
 
+int	ft_recursive_factorial(int nb)
+{
 	if (nb < 0)
 		return (0);
-	i = 1;
-	ret = 1;
-	while (i <= nb)
-	{
-		ret = i * ret;
-		++i;
-	}
-	return (ret);
+	return (rec(nb));
 }
